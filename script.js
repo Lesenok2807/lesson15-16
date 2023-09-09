8ec6b2d5f6e479c3d2a06071ff8fcdd339a931fe
Olesia Ivanova <ivanova.ov_87@mail.ru>
2023-09-09 22:51:06 +0300

    первый коммит к 16 уроку

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
 'use sctrict';
 
-const DomElement = function(selectorValue, heightValue, widthValue, bgValue, fontSizeValue) {
-    this.selector = selectorValue;
-    this.height = heightValue + 'px';
-    this.width = widthValue + 'px';
-    this.bg = bgValue;
-    this.fontSize = fontSizeValue + 'px';
-    this.addElement = function(str) {
-       if (selectorValue[0] === '.') {        
-            let div = document.createElement('div');
-            let selectorCopy = selectorValue.slice(1);
+class First {
+     constructor(text) {
+          this.text = text;
+     }
 
-            div.className = selectorCopy;
-            document.body.appendChild(div);
+     hello() {
+          console.log('Привет я метод родителя!');
+     }
+}
 
-       } else if (selectorValue[0] === '#') {        
-            let selectorCopy = selectorValue.slice(1);
-            let elementNew = document.createElement('p');
+class Second extends First {
+     constructor(text, skills = []) {
+          super(text)
+          this.skills = skills;
+          console.log(text);
+     }
+}
 
-            elementNew.id = selectorCopy;
-            elementNew.textContent = str;
-            elementNew.style.cssText = `height : ${this.height}; width : ${this.width}; background : ${this.bg}; font-size : ${this.fontSize}`;
-            document.body.appendChild(elementNew);
-            console.dir(elementNew);
-       } else {
-            alert('Ввод начинать с "." либо "#');
-       }
-    };
-};
+const third = new Second('А я наследуемый метод!');
 
-let text = prompt('Введите текст');
-
-let fitstElememt = new DomElement('.block', 200, 200, 'blue', 22);
-fitstElememt.addElement(text);
-let secondElememt = new DomElement('#best', 200, 200, 'blue', 52);
-secondElememt.addElement(text);
+third.hello();
